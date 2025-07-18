0b41cc39fb334e1d2a720d1a5ae3fc7928e72700
shruthika-tr <shruthikaramesh2004@gmail.com>
2025-07-18 16:40:29 +0530

    Initial commit of portfolio site

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,31 +2,37 @@ import ResumeImg from "../assets/resume.jpg";
 
 export default function Resume() {
   const config = {
-    link: "https://image.slidesharecdn.com/ac9bf358-b4c9-4acf-b78b-df999278b914-160510022209/95/mariecriseulloneresume-1-638.jpg?cb=1462846946",
+    link: "/resume.pdf", // 👈 make sure this is inside the public folder
   };
 
   return (
     <section
       id="resume"
-      className="flex flex-col md:flex-row bg-secondary px-5"
+      className="flex flex-col md:flex-row bg-secondary px-5 py-10"
     >
-      <div className="py-5 md:w-1/2 flex justify-center md: justify-end">
-        <img className="w-[300px]" src={ResumeImg} alt="Resume" />
+      {/* Left Side: Resume Image */}
+      <div className="md:w-1/2 flex justify-center md:justify-end mb-5 md:mb-0">
+        <img className="w-[300px] rounded shadow-lg" src={ResumeImg} alt="Resume" />
       </div>
+
+      {/* Right Side: Download Section */}
       <div className="md:w-1/2 flex justify-center">
-        <div className="flex flex-col justify-center text-white">
-          <h1 className="text-4xl border-b-4 border-primary mb-5 w-[140px] font-bold">
+        <div className="flex flex-col justify-center text-white max-w-md">
+          <h1 className="text-4xl border-b-4 border-primary mb-5 w-fit font-bold">
             Resume
           </h1>
           <p className="pb-5">
-            You can view my resume
-            <a
-              className="btn ml-2 px-3 py-1 bg-primary text-white rounded"
-              href={config.link}
-            >
-              Download
-            </a>
+            This is my beginner-level resume created during the early stage of my career. Feel free to view or download it below:
           </p>
+          <a
+            className="inline-block px-4 py-2 bg-primary text-white font-semibold rounded hover:bg-white hover:text-primary transition duration-300 ease-in-out w-fit"
+            href={config.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            download
+          >
+            📄 Download Resume
+          </a>
         </div>
       </div>
     </section>
